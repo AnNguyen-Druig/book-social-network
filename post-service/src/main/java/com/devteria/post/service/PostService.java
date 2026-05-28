5c006d990da2b16c3ea08d8144192575a2849f22
AnNguyen-Druig <nguyentrandatan@gmail.com>
2026-05-28 14:01:20 +0700

    feat(course/initial-get-profile-info): get profile info from profile-service to post-service (add username)

--- post-service/src/main/java/com/devteria/post/service/PostService.java
+++ post-service/src/main/java/com/devteria/post/service/PostService.java
@@ -3,12 +3,15 @@ package com.devteria.post.service;
 import com.devteria.post.dto.PageResponse;
 import com.devteria.post.dto.request.PostRequest;
 import com.devteria.post.dto.response.PostResponse;
+import com.devteria.post.dto.response.UserProfileResponse;
 import com.devteria.post.entity.Post;
 import com.devteria.post.mapper.PostMapper;
 import com.devteria.post.repository.PostRepository;
+import com.devteria.post.repository.httpclient.ProfileClient;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -22,10 +25,12 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class PostService {
     DateTimeFormatter dateTimeFormatter;
     PostRepository postRepository;
     PostMapper postMapper;
+    ProfileClient profileClient;
 
     public PostResponse createPost(PostRequest request){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -45,13 +50,24 @@ public class PostService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String userId = authentication.getName();
 
+        UserProfileResponse userProfile = null;
+        try {
+            userProfile = profileClient.getUserProfile(userId).getResult(); //getResult() sẽ lấy về object UserProfileResponse
+        } catch (Exception e) {
+            log.error("Error while getting user profile", e);
+        }
+
         Sort sort = Sort.by("createdDate").descending();
         Pageable pageable = PageRequest.of(page - 1, size, sort);
         var pageData = postRepository.findAllByUserId(userId, pageable);
 
+        //Take username from userProfile from ProfileClient from profile-service
+        String username = userProfile != null ? userProfile.getUsername() : null;
+
         var postList = pageData.getContent().stream().map(post -> {
             var postResponse = postMapper.toPostResponse(post);
             postResponse.setCreated(dateTimeFormatter.format(post.getCreatedDate()));
+            postResponse.setUserName(username);
             return postResponse;
         }).toList();
 
